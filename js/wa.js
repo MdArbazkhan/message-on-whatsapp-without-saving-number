f5543ea121263d0941b5423539a78d3bebd464f1
Arbaz Khan <86012455+MdArbazkhan@users.noreply.github.com>
2021-06-25 22:53:18 +0530

    Added user interface based on user recommendation.

--- js/wa.js
+++ js/wa.js
@@ -1,19 +1,25 @@
-// reading form field
+//reading form field
 const inputFieldElement = document.getElementById("input-field");
 const btnElement = document.getElementById("btn");
 
+//creating link
 const waLink = "https://api.whatsapp.com/send?phone=";
 const waLink2 = "&text";
 
 
 
-
+//functionality
 const onBtnClick = () => {
    
     const formInputField = inputFieldElement.value;
     const link = waLink + formInputField + waLink2;
     window.location.replace(link);
+    inputFieldElement.value = '';
+    formInputField.value = '';
 
 }
 
 btnElement.addEventListener('click' , onBtnClick);
+
+
+
